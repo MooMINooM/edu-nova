8784166587845b805e6660b52431808c5a51b174
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-01 21:51:07 +0700

    Configure image hostnames and fix URLs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // <-- อนุญาต Hostname นี้
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com', // <-- อนุญาตเผื่อไว้ด้วย
+      }
+    ],
+  },
+};
 
 export default nextConfig;
